8996240ea0cbc8e4b6a45442a07fc73b446b9fad
INDRA\eperedaa <Tercera3!>
2024-11-13 10:16:13 +0100

    Limpiado gestor de tareas

--- src/gestorUsuarios/gestorUsuarios.ts
+++ src/gestorUsuarios/gestorUsuarios.ts
@@ -1,4 +1,5 @@
 // Definir roles simples, como Administrador y Colaborador, usando `Enums`.
-//    - El rol de Administrador puede gestionar todas las tareas, mientras que el Colaborador solo puede visualizar y modificar las tareas asignadas.
+//    - El rol de Administrador puede gestionar todas las tareas, 
+//     mientras que el Colaborador solo puede visualizar y modificar las tareas asignadas.
 
 
