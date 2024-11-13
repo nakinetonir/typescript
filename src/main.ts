8b09e1d14f9f14cc6732071313e562ced79aadce
INDRA\eperedaa <Tercera3!>
2024-11-13 12:11:08 +0100

    Integracion

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { gestorTareas } from './gestorTareas/gestorTareas';
 import { Usuario } from './models/usuario';
 import { Roles } from './models/roles';
 import { gestorUsuarios } from './gestorUsuarios/gestorUsuarios';
+import { UsuarioService } from './services/usuariosServices';
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -11,9 +12,9 @@ const rl = readline.createInterface({
 
 // Gestión de usuario, hay que crear algunos usuarios.
 //const usuarioEnCurso: Usuario;
-// TODO: Arreglar la gestión de usuarios
-const usuarioEnCurso: Usuario = new Usuario("Eneko", Roles.Administrador);
+//const usuarioEnCurso: Usuario = new Usuario("Eneko", Roles.Administrador);
 
 gestorUsuarios.mostrarMenu();
+
 // Llamada a mostrarMenu con el usuario creado
-gestorTareas.mostrarMenu(usuarioEnCurso);
+gestorTareas.mostrarMenu(UsuarioService.getUsuario());
